83c9d5cab2988dd261902fc842fe1fb5312a0720
Mahendra Chari <mahi.chari2010@gmail.com>
2023-04-29 20:25:11 +0530

    modified Cart pop over

---
+++ src/Utils/Utils.ts
@@ -0,0 +1,7 @@
+export const formatNumber=(num:number)=>{
+    //format to  2 digits
+    return(
+        num.toFixed(2)
+    )
+
+}
--- src/pages/Cart/CartPopOver.tsx
+++ src/pages/Cart/CartPopOver.tsx
@@ -5,6 +5,9 @@ import {FaTrash} from "react-icons/fa"
 import { useDispatch } from "react-redux";
 import { removeFromCart,clearCart } from "./cartSlice";
 import { prodDataType } from "../../types/productDataType";
+import Table from "react-bootstrap/Table";
+import { useMemo } from "react";
+import {formatNumber} from "../../Utils/Utils"
 import  './Cart.scss'
 
 const CartPopOver =(props:any)=>{
@@ -14,32 +17,56 @@ const CartPopOver =(props:any)=>{
         setOpen(false);
         dispatch(clearCart());
     }
-    const totalOrderPrice=
-        props.items.length >0  ? props.items.map(
-            (item:prodDataType)=>item.price).reduce(
-                (sum=0,cur:any)=>sum+cur
-            ) : 0;
+
+    const totalOrderPrice= useMemo(
+        ()=>
+            props.items.length >0  ? props.items.map(
+                (item:prodDataType)=>item.price).reduce(
+                    (sum=0,cur:any)=>sum+cur
+                ) : 0
+        ,[props.items]
+    )
         
     return(
         <>
             <div className="cart-icon" onClick={()=>setOpen(true)}><FaShoppingCart/><sup>{props.items.length}</sup></div>
             <Modal show={open} onHide={()=>setOpen(false)}>
                 <Modal.Header closeButton> Cart Details</Modal.Header>
-                <Modal.Body>{props.items.map((item:any)=>{
-
-                    return(<div>
-                            <div className="item-detail">
-                                <img src={item.image} className="item-image img-responsive" alt={item.title}/>
-                                <div>{item.title}</div>
-                                <div className="price">Price:${item.price}</div>
-                                <a className="close-cart-item" onClick={()=>dispatch(removeFromCart(item))}><FaTrash/></a>
-                            </div>
-                    </div>)
-
-                    })}
-                    <div className="total-value">{`Total Order Price: $ ${totalOrderPrice} `}</div>
+                <Modal.Body>
+                    <Table striped>
+                        <thead>
+                            <th>#</th>
+                            <th>Item</th>
+                            <th>Title</th>
+                            <th>MRP</th>
+                            <th>Discount</th>
+                            <th>Price</th>
+                        </thead>
+                        <tbody>
+                            {props.items.map((item:prodDataType,index:number)=>{
+                                return(<tr>
+                                    <td>{index+1}</td>
+                                    <td><img src={item.image} className="item-image img-responsive" alt={item.title}/></td>
+                                    <td>{item.title}</td>
+                                    <td>{formatNumber(item.price)}</td>
+                                    <td>{"60%"}</td>
+                                    <td>{formatNumber(item.price * 0.6)}</td>
+                                </tr>)
+                                
+                            })}
+                            <tr  className="total-row">
+                                <td></td>
+                                <td></td>
+                                <td>Total</td>
+                                <td>{totalOrderPrice}</td>
+                                <td>{"60%"}</td>
+                                <td>{formatNumber(totalOrderPrice * 0.6)}</td>
+                            </tr>
+                        </tbody>
+                    </Table>
                 </Modal.Body>
                 <Modal.Footer>
+                    <div className="savedMsg">{`You have Saved $${formatNumber(totalOrderPrice*0.4)}`}</div>
                     <Button onClick={handlePlaceOrder}>Place Order</Button>
                 </Modal.Footer>
             </Modal>
